f91833174e9f88006033133855c6a50f7c7c3c6f
cameronhum <75474227+cameronhum@users.noreply.github.com>
2022-03-21 22:54:40 +1100

    Add files via upload

---
+++ blink-an-led.ino
@@ -0,0 +1,197 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "lib1.h"
+
+
+const pin_t MY_LED = D7;
+
+
+SYSTEM_THREAD(ENABLED);
+
+void setup()
+{
+	
+	pinMode(MY_LED, OUTPUT);
+}
+
+void loop()
+{
+ 
+    
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, LOW);
+
+	delay(1500ms);
+
+	digitalWrite(MY_LED, HIGH);
+
+	delay(1500ms);
+	
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, HIGH);
+
+	delay(1500ms);
+	
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(1500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, HIGH);
+
+	delay(1500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(1500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(1500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(1500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(1500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(1500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(1500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+	
+	digitalWrite(MY_LED, HIGH);
+
+	delay(500ms);
+
+	digitalWrite(MY_LED, LOW);
+
+	delay(500ms);
+
+}
